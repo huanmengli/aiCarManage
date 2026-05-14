d4cbcc70d9e5e598f012a906e9a1a31041efcc8e
Admin <18266753850@163.com>
2026-05-14 21:18:53 +0800

    初始化架构搭建

---
+++ src/main/java/com/liuyan/AiCarManageApplication.java
@@ -0,0 +1,13 @@
+package com.liuyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCarManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCarManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liuyan/AiCarManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCarManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
